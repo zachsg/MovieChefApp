1b9b0d7f699e6f19d12ab6bb15c1444cb90c32ea
zachsg <zsgott@gmail.com>
2017-05-08 16:00:19 -0700

    Finish parsing of JSON into array of Movie objects

--- app/src/main/java/com/example/android/moviechef/utilities/JsonUtils.java
+++ app/src/main/java/com/example/android/moviechef/utilities/JsonUtils.java
@@ -20,7 +20,7 @@ public final class JsonUtils {
         // Base level array of all movies returned by query
         final String RESULTS = "results";
 
-        Movie[] movies;
+        Movie[] movies = null;
 
         JSONObject moviesJsonFull = new JSONObject(moviesString);
         if (moviesJsonFull.has(RESULTS)) {
@@ -28,8 +28,48 @@ public final class JsonUtils {
             JSONArray moviesJsonArray = moviesJsonFull.getJSONArray(RESULTS);
 
             for (int i = 0; i < moviesJsonArray.length(); i++) {
+                String title = "";
+                String imageUrl = "";
+                String overview = "";
+                String id = "";
 
+                // Get a single movie
+                JSONObject jsonMovie = moviesJsonArray.getJSONObject(i);
+
+
+                title = setValueForKey(jsonMovie, "title");
+                imageUrl = setValueForKey(jsonMovie, "poster_path");
+                overview = setValueForKey(jsonMovie, "overview");
+                id = setValueForKey(jsonMovie, "id");
+
+                Movie movie = new Movie(title, imageUrl, overview, id);
+                try {
+                    movies[i] = movie;
+                } catch (NullPointerException npe) {
+                    npe.printStackTrace();
+                    movies[i] = null;
+                }
             }
+        }
+        return movies;
+    }
+
+    /**
+     * Take in movie JSON object and return the value for the key.
+     * @param movie JSON object containing movie details.
+     * @param key The key to be looked up in the movie JSON object.
+     * @return The value for the key in the given JSON movie object.
+     */
+    private static String setValueForKey(JSONObject movie, String key) {
+        if (movie.has(key)) {
+            try {
+                return movie.getString(key);
+            } catch (JSONException jse) {
+                jse.printStackTrace();
+                return "n/a";
+            }
+        } else {
+            return "n/a";
         }
     }
 }
