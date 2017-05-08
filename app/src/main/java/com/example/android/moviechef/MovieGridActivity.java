c19f552e2786b167745fd474a695eac431168ddd
zachsg <zsgott@gmail.com>
2017-05-07 20:06:56 -0700

    Add menu & better AsyncTask handling

--- app/src/main/java/com/example/android/moviechef/MovieGridActivity.java
+++ app/src/main/java/com/example/android/moviechef/MovieGridActivity.java
@@ -6,6 +6,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.example.android.moviechef.utilities.NetworkUtils;
 
 public class MovieGridActivity extends AppCompatActivity
                             implements MovieAdapter.MovieAdapterOnClickHandler {
@@ -18,6 +23,9 @@ public class MovieGridActivity extends AppCompatActivity
     private MovieAdapter mMovieAdapter;
 
     private static final int SPAN_COUNT = 2;
+
+    private static final String POPULAR_MOVIES = "/movie/popular";
+    private static final String TOP_RATED_MOVIES = "/movie/top_rated";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,11 +51,40 @@ public class MovieGridActivity extends AppCompatActivity
         // TODO: do stuff when movie clicked ;)
     }
 
-    private class FetchMoviesTask extends AsyncTask<Movie, Void, Movie[]> {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.movies_grid, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.sort_popular:
+                new FetchMoviesTask().execute(POPULAR_MOVIES);
+                break;
+            case R.id.sort_top_rated:
+                new FetchMoviesTask().execute(TOP_RATED_MOVIES);
+                break;
+            default:
+                return false;
+        }
+        return true;
+    }
+
+    private class FetchMoviesTask extends AsyncTask<String, Void, Movie[]> {
 
         @Override
-        protected Movie[] doInBackground(Movie... params) {
-            return new Movie[0];
+        protected Movie[] doInBackground(String... params) {
+            // If sort by pop/top rated was requested, build that into URL
+            if (params.length == 1) {
+                String url = NetworkUtils.buildUrl(params[0]);
+            } else {
+                String url = NetworkUtils.buildUrl(null);
+            }
+
+            return null;
         }
 
         @Override
--- app/src/main/java/com/example/android/moviechef/utilities/NetworkUtils.java
+++ app/src/main/java/com/example/android/moviechef/utilities/NetworkUtils.java
@@ -1,5 +1,11 @@
 package com.example.android.moviechef.utilities;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
 /**
  * Utilities for communicating with themoviedb.org.
  */
@@ -13,5 +19,45 @@ public final class NetworkUtils {
 
     // Be sure to add your API_KEY here
     private static final String API_KEY = "";
+
     private static final String MOVIES_BASE_URL = MOVIES_STARTER_URL + API_KEY;
+
+    private static final String POPULAR_MOVIES = "/movie/popular";
+    private static final String TOP_RATED_MOVIES = "/movie/top_rated";
+
+    public static String buildUrl(String sort) {
+        if (sort.equals(POPULAR_MOVIES)) {
+            return MOVIES_BASE_URL + POPULAR_MOVIES;
+        } else if (sort.equals(TOP_RATED_MOVIES)) {
+            return MOVIES_BASE_URL + TOP_RATED_MOVIES;
+        } else {
+            return MOVIES_BASE_URL;
+        }
+    }
+
+    /**
+     * This method returns the entire result from the HTTP response.
+     *
+     * @param url The URL to fetch the HTTP response from.
+     * @return The contents of the HTTP response.
+     * @throws IOException Related to network and stream reading
+     */
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
 }
